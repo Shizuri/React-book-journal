06b8b60a85d790e639c79f94a1b02e5c6e84164b
Zdravko Mavkov <mavkov@hotmail.com>
2020-02-25 09:44:35 +0100

    added React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import './App.css'
 import FindBook from './FindBook'
+import Journal from './Journal'
+import { NavLink, Route, Switch, Redirect } from 'react-router-dom'
 
 function App() {
 	return (
 		<div className='App'>
-			<FindBook />
+			<nav style={{paddingBottom: '20px'}}>
+				<NavLink to='/' style={{paddingRight: '20px'}}>Find Book</NavLink>
+				<NavLink to='/journal'>Book Journal</NavLink>
+			</nav>
+
+			<Switch>
+				<Route exact path='/'>
+					<FindBook />
+					{/* <Redirect exact from="/" to="search" /> */}
+				</Route>
+				<Route exact path='/journal'>
+					<Journal />
+				</Route>
+			</Switch>
 		</div>
 	)
 }
