b068654c2777208af2176295375849fef2b57d10
Zdravko Mavkov <mavkov@hotmail.com>
2020-03-05 12:45:10 +0100

    Working on EditJournalEntry

--- src/EditJournalEntry.js
+++ src/EditJournalEntry.js
@@ -1,13 +1,87 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useHistory, useLocation } from 'react-router-dom'
 
 const EditJournalEntry = props => {
-    const history = useHistory() // Browsing history provided by react-router
+    // Browsing history provided by react-router
+    const history = useHistory()
+    // Get the book information passed from JournalEntry as a Link state prop from react-router
+    const { state: bookState } = useLocation()
+    const { bookId, bookTitle, bookThumbnail } = bookState.book
+
+    const [startDate, setStartDate] = useState('')
+    const [finishDate, setFinishDate] = useState('')
+    const [review, setReview] = useState('')
+    const [rating, setRating] = useState(-1)
+    const [notes, setNotes] = useState('')
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        console.log('startDate: ', startDate)
+        console.log('finishDate: ', finishDate)
+        console.log('rating: ', rating)
+        console.log('review: ', review)
+        console.log('notes: ', notes)
+    }
 
     return (
         <div>
-            <button onClick={history.goBack}>Back to Journal Entry</button>
-            BLAH!
+            <p>Review value: {review}</p>
+            <form onSubmit={handleSubmit}>
+                <label>
+                Started reading on:
+                    <input
+                        type='date'
+                        name='startDate'
+                        value={startDate}
+                        onChange={event => setStartDate(event.target.value)}
+                    />
+                </label>
+                <br />
+                <label>
+                Finished reading on:
+                    <input
+                        type='date'
+                        name='finishDate'
+                        value={finishDate}
+                        onChange={event => setFinishDate(event.target.value)}
+                    />
+                </label>
+                <br />
+                <label>
+                    My rating:
+                    <input
+                        type='number'
+                        name='rating'
+                        value={rating}
+                        onChange={event => setRating(event.target.value)}
+                        min='1'
+                        max='5'
+                    />
+                </label>
+                <br />
+                <label>
+                    My review:
+                    <input
+                        type='text'
+                        name='review'
+                        value={review}
+                        onChange={event => setReview(event.target.value)}
+                    />
+                </label>
+                <br />
+                <label>
+                Additional notes:
+                    <input
+                        type='text'
+                        name='notes'
+                        value={notes}
+                        onChange={event => setNotes(event.target.value)}
+                    />
+                </label>
+                <br />
+                <input type='submit' value='Submit changes' />
+            </form>
+            <button onClick={() => console.log('Under construction')}>Cancel changes</button>
         </div>
     )
 }
--- src/JournaEntryDetails.js
+++ src/JournaEntryDetails.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
 const JournaEntryDetails = props => {
-    // Get the book information passed from Journal as a Link state prop from react-router
+    // Get the book information passed from JournalEntry as a Link state prop from react-router
     const { state: bookState } = useLocation()
     const { bookId, bookTitle, bookThumbnail } = bookState.book
 
@@ -13,13 +13,13 @@ const JournaEntryDetails = props => {
             <p>Started reading on: </p>
             <p>Finished reading on: </p>
             <p>My rating: </p>
-            <p>My opinion: </p>
+            <p>My review: </p>
             <p>Additional notes: </p>
             <Link to={{
                 // Send the pathname and the book information to the clicked link
                 pathname: `edit/${bookId}`,
                 state: {
-                    book: bookState
+                    book: bookState.book
                 }
             }}> Add / Edit Journal Entry</Link>
             <button>Remove Book and Entry from Journal</button>
