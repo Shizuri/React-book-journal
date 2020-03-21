085f15800b85c26f207140a38c1a20650ee3bc57
Zdravko Mavkov <mavkov@hotmail.com>
2020-03-21 11:57:32 +0100

    Finished styling BookDetails

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -70,15 +70,15 @@ const BookDetails = props => {
     // Because of inconsistency issues with the Google Books API some requests require a bit of management 
     const industryIdentifiersPrintout = () => {
         if (!book.industryIdentifiers) {
-            return <p>Industry Identifiers: <i>Industry Identifiers not available</i></p>
+            return <p><span className='BookDetails-descriptor'>Industry Identifiers:</span> <i>Industry Identifiers not available</i></p>
         } else if (book.industryIdentifiers[0].type === 'OTHER') {
-            return <p>Industry identifier: {book.industryIdentifiers[0].identifier}</p>
+            return <p><span className='BookDetails-descriptor'>Industry identifier:</span> {book.industryIdentifiers[0].identifier}</p>
         } else {
             return (
                 <>
-                    <p>Industry Identifiers:</p>
-                    <p>ISBN 10: {book.industryIdentifiers ? book.industryIdentifiers.filter(isbn => isbn.type === 'ISBN_10')[0].identifier : <i>ISBN 10 not available</i>}</p>
-                    <p>ISBN 13: {book.industryIdentifiers ? book.industryIdentifiers.filter(isbn => isbn.type === 'ISBN_13')[0].identifier : <i>ISBN 13 not available</i>}</p>
+                    <p><span className='BookDetails-descriptor'>Industry Identifiers:</span> </p>
+                    <p><span className='BookDetails-descriptor'>ISBN 10:</span> {book.industryIdentifiers ? book.industryIdentifiers.filter(isbn => isbn.type === 'ISBN_10')[0].identifier : <i>ISBN 10 not available</i>}</p>
+                    <p><span className='BookDetails-descriptor'>ISBN 13:</span> {book.industryIdentifiers ? book.industryIdentifiers.filter(isbn => isbn.type === 'ISBN_13')[0].identifier : <i>ISBN 13 not available</i>}</p>
                 </>
             )
         }
@@ -86,27 +86,27 @@ const BookDetails = props => {
 
     return (
         <div className='BookDetails'>
-            <h2>Book details about <i>{book.title}</i></h2>
+            <h3>Book details about <i>{book.title}</i></h3>
             <div className='BookDetails-container'>
                 <div className='BookDetails-left-panel'>
-                    <img src={img} alt={book.title} />
+                    <img src={img} alt={book.title} className='BookDetails-img'/>
                     {bookIsInJournal ?
-                        <div>Book is in Journal</div>
-                        : <button onClick={() => addBookToJournal({ id: bookId, title: book.title, img })}>Add to Journal</button>}
+                        <div className='BookDetails-is-in-Journal'>Book is in Journal</div>
+                        : <button onClick={() => addBookToJournal({ id: bookId, title: book.title, img })} className='BookDetails-add-to-journal-button'>Add to Journal</button>}
                 </div>
                 <div className='BookDetails-right-panel'>
-                    <p>Title: {book.title}</p>
-                    <p>Subtitle: {subtitle}</p>
-                    <p>Authors: {authors}</p>
-                    <p>Publisher: {publisher}</p>
-                    <p>Date of publishing: {dateOfPublishing}</p>
-                    <p>Description: {description}</p>
+                    <p><span className='BookDetails-descriptor'>Title:</span> {book.title}</p>
+                    <p><span className='BookDetails-descriptor'>Subtitle:</span> {subtitle}</p>
+                    <p><span className='BookDetails-descriptor'>Authors:</span> {authors}</p>
+                    <p><span className='BookDetails-descriptor'>Publisher:</span> {publisher}</p>
+                    <p><span className='BookDetails-descriptor'>Date of publishing:</span> {dateOfPublishing}</p>
+                    <p><span className='BookDetails-descriptor'>Description:</span> {description}</p>
                     {industryIdentifiersPrintout()}
-                    <p>Page count: {pageCount}</p>
-                    <p>Categories: {categories}</p>
-                    <p>User rating: {userRatings}</p>
-                    <p>Мaturity rating: {maturityRating}</p>
-                    <p>Language: {language}</p>
+                    <p><span className='BookDetails-descriptor'>Page count:</span> {pageCount}</p>
+                    <p><span className='BookDetails-descriptor'>Categories:</span> {categories}</p>
+                    <p><span className='BookDetails-descriptor'>User rating:</span> {userRatings}</p>
+                    <p><span className='BookDetails-descriptor'>Мaturity rating:</span> {maturityRating}</p>
+                    <p><span className='BookDetails-descriptor'>Language:</span> {language}</p>
                 </div>
             </div>
             <button onClick={history.goBack} className='BookDetails-back-button'>Back</button>
