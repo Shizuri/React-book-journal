a5a58801120e7ffd1378b0d254d41e69fe1f93d5
Zdravko Mavkov <mavkov@hotmail.com>
2020-04-18 15:12:36 +0200

    Modified JournalEntryDetails.js to read myBooks from contextAPI instead of localStorage

--- src/JournalEntryDetails.js
+++ src/JournalEntryDetails.js
@@ -11,7 +11,9 @@ const JournalEntryDetails = props => {
     // Get the book id that is sent as the book parameter in the URL
     const { bookId } = useParams()
     let bookTitle, bookThumbnail
-    const myBooks = JSON.parse(localStorage.getItem('books') || '[]')
+
+    // Get the removeBookFromJournal and myBooks function needed from journalContext
+    const { myBooks, removeBookFromJournal } = useContext(JournalContext)
     try {
         ({ bookTitle, bookThumbnail } = myBooks.filter(book => book.bookId === bookId)[0])
     } catch (error) {
@@ -20,9 +22,6 @@ const JournalEntryDetails = props => {
 
     // Needed to redirect back to /journal after the book has been removed
     const history = useHistory()
-
-    // Get the removeBookFromJournal function needed from journalContext
-    const { removeBookFromJournal } = useContext(JournalContext)
 
     // State for display data
     const [startDate, setStartDate] = useState('')
